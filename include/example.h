f22a61c55b51991f340e86870fbb652d05e01a52
choushunn <choushunn@stu.cqut.edu.cn>
2024-07-05 22:31:31 +0800

    add:vtk

--- include/example.h
+++ include/example.h
@@ -1,5 +1,9 @@
 #pragma once
 #include <spdlog/spdlog.h>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include "vtk_test.h"
 
 void example_sqlite3(){
 
@@ -8,5 +12,36 @@ void example_sqlite3(){
 void example_spdlog(){
     spdlog::info("info");
     spdlog::critical("critical");
+}
 
+
+int test_opencv_videoio(){
+
+    // 创建一个 VideoCapture 对象来访问默认摄像头
+    cv::VideoCapture cap(0);
+
+    // 检查摄像头是否成功打开
+    if (!cap.isOpened())
+    {
+        spdlog::error("Failed to open default camera");
+        return -1;
+    }
+    cv::Mat frame;
+    // 无限循环读取和显示视频帧
+    while (true)
+    {
+
+        cap >> frame;
+
+        // 显示视频帧
+        cv::imshow("Frame", frame);
+
+        // 按下 'q' 键退出循环
+        if (cv::waitKey(1) == 'q')
+            break;
+    }
+
+    // 释放资源
+    cap.release();
+    cv::destroyAllWindows();
 }
---
+++ include/vtk_test.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <vtkActor.h>
+#include <vtkAutoInit.h>
+#include <vtkConeSource.h>
+#include <vtkCubeSource.h>
+#include <vtkCylinderSource.h>
+#include <vtkDiskSource.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkSmartPointer.h>
+#include <vtkSphereSource.h>
+#include <vtkVideoSource.h>
+VTK_MODULE_INIT(vtkRenderingOpenGL2);
+VTK_MODULE_INIT(vtkInteractionStyle);
+VTK_MODULE_INIT(vtkRenderingFreeType)
+
+int test_vtk()
+{
+    //圆锥模型
+    vtkSmartPointer<vtkConeSource> cone = vtkSmartPointer<vtkConeSource>::New();
+    cone->SetHeight(3.0);
+    cone->SetRadius(1.0);
+    cone->SetResolution(10);
+    vtkNew<vtkDiskSource> diskSource;
+    diskSource->SetInnerRadius(0.5);
+    diskSource->SetOuterRadius(1.0);
+    diskSource->SetRadialResolution(20);
+    diskSource->SetCircumferentialResolution(20);
+
+    vtkNew<vtkCylinderSource> cylinderSource;
+    cylinderSource->SetRadius(1.0);
+    cylinderSource->SetHeight(3.0);
+    cylinderSource->SetResolution(20);
+    vtkNew<vtkCubeSource> cubeSource;
+    cubeSource->SetXLength(2.0);
+    cubeSource->SetYLength(2.0);
+    cubeSource->SetZLength(2.0);
+    vtkNew<vtkSphereSource> sphereSource;
+    sphereSource->SetRadius(1.0);
+    sphereSource->SetPhiResolution(20);
+    sphereSource->SetThetaResolution(20);
+    //映射器
+    vtkSmartPointer<vtkPolyDataMapper> coneMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    coneMapper->SetInputConnection(cubeSource->GetOutputPort());
+
+    //对象
+    vtkSmartPointer<vtkActor> coneActor = vtkSmartPointer<vtkActor>::New();
+    coneActor->SetMapper(coneMapper);
+
+    //渲染
+    vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+    renderer->AddActor(coneActor);
+    renderer->SetBackground(0.0, 1.0, 1.0);
+
+    //渲染窗口
+    vtkSmartPointer<vtkRenderWindow> renWin = vtkSmartPointer<vtkRenderWindow>::New();
+    renWin->AddRenderer(renderer);
+    renWin->SetSize(600, 400);
+
+    //交互
+    vtkSmartPointer<vtkRenderWindowInteractor> renderInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    renderInteractor->SetRenderWindow(renWin);
+
+    //交互风格
+    vtkSmartPointer<vtkInteractorStyleTrackballCamera> style =
+        vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New();
+    renderInteractor->SetInteractorStyle(style);
+
+    renderInteractor->Initialize();
+    renderInteractor->Start();
+
+    return 0;
+}
