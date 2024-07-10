1161df04577cfa0b147d1174d36457d859e5fd0b
choushunn <choushunn@stu.cqut.edu.cn>
2024-07-10 22:34:07 +0800

    fix:sh

--- build-aarch64-linux.sh
+++ build-aarch64-linux.sh
@@ -1,28 +1,31 @@
 set -e
 
-# 交叉编译
- #GCC_COMPILER=/usr/local/arm64/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu
-
-#GCC_COMPILER=/opt/toolchain/aarch64-rockchip1031-linux-gnu/bin/aarch64-none-linux-gnu
-
-#export CC=${GCC_COMPILER}-gcc
-#export CXX=${GCC_COMPILER}-g++
+# 交叉编译设置
+CROSS_COMPILE="$1"
+if [ "$CROSS_COMPILE" == "aarch64" ]; then
+    CROSS_COMPILE=1
+    GCC_COMPILER=/opt/toolchain/aarch64-rockchip1031-linux-gnu/bin/aarch64-none-linux-gnu
+    export CC=${GCC_COMPILER}-gcc
+    export CXX=${GCC_COMPILER}-g++
+fi
 
 ROOT_PWD=$( cd "$( dirname $0 )" && cd -P "$( dirname "$SOURCE" )" && pwd )
 
-# build
+# 构建目录
 BUILD_DIR=${ROOT_PWD}/build
-
 if [ ! -d "${BUILD_DIR}" ]; then
   mkdir -p ${BUILD_DIR}
 fi
 
 cd ${BUILD_DIR}
 
-cmake ../ -DCMAKE_SYSTEM_NAME=Linux
-make -j8
+# 配置和构建
+if [ "$CROSS_COMPILE" == "aarch64" ]; then
+    cmake -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_PROCESSOR=aarch64 -DCMAKE_INSTALL_PREFIX=./install ..
+else
+    cmake -DCMAKE_INSTALL_PREFIX=./install ..
+fi
 
+make -j${nproc}
 make install
 cd -
-
-
